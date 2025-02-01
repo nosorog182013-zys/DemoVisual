a3227592440b882178460d382c3890169fd398e1
nosorog182013-zys <nosorog182013@gmail.com>
2025-02-01 22:43:37 +0800

    sphinx

---
+++ source/conf.py
@@ -0,0 +1,58 @@
+import datetime
+#import ast
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Дипломная работа'
+author = 'Серней Зацаринный'
+copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+ogp_site_url = "https://canonical-starter-pack.readthedocs-hosted.com/"
+ogp_site_name = project
+ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
+
+html_context = {
+    "github_folder": "/DemoVisual/",
+    "display_contributors": False,
+    "sequential_nav": "both",
+}
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['source/_static']
+templates_path = ['source/_templates']
+# Adds custom JavaScript files, located under 'html_static_path'
+# html_js_files = []
+# Adds custom CSS files, located under 'html_static_path'
+#html_css_files = [
+#    "css/pdf.css",
+#]
+
+extensions = [
+    "canonical_sphinx",
+    "sphinxcontrib.cairosvgconverter",
+    "sphinx_last_updated_by_git",
+]
+
+exclude_patterns = [
+    "doc-cheat-sheet*",
+]
+
+
+redirects = {}
+
+disable_feedback_button = True
---
+++ source/index.rst
@@ -0,0 +1,26 @@
+.. Дипломная работа documentation master file, created by
+   sphinx-quickstart on Sat Feb  1 20:35:58 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Дипломная работа documentation
+==============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+    :maxdepth: 2
+    #:caption: Contents:
+    /docs/patent/introduction
+    /docs/patent/choice
+    /docs/patent/matplotlib
+    /docs/patent/seaborn
+    /docs/patent/plotly
+    /docs/patent/justific
+    /docs/patent/rec
+    /docs/patent/analysis
+    /docs/patent/conclusion
+
