c41e3f176d1e0832e865ca4ee4836c15e98ec197
nosorog182013-zys <nosorog182013@gmail.com>
2025-02-03 16:24:42 +0800

    sphinx

--- description/_sources/index.rst.txt
+++ description/_sources/index.rst.txt
@@ -14,6 +14,6 @@ documentation for details.
 .. toctree::
     :maxdepth: 2
 
-    ../src/usage/validata
+    ./validata
 
 
--- src/validata.py
+++ src/validata.py
@@ -1,3 +1,10 @@
+"""
+Программа DemoVisual демонстрирует 3 графика финансовых данных в виде так называемых
+свечей. Виджет первый использует Matplotlib, второй Seaborn, третий Plotly. Данные
+представлены в файле.csv, изменение цены BTC/USD и объема BTC. Цель - продемонстрировать
+возможности библиотек Matplotlib, Seaborn, Plotly по рисованию одних и тех же данных
+для сравнения.
+"""
 import pandas as pd
 # Import a tool for structuring data.
 file_path = 'dataset/test_data.csv'
