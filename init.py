13db7dbc1ebd3b54dd826d16df25b6b68a396446
nosorog182013-zys <nosorog182013@gmail.com>
2025-02-03 19:57:27 +0800

    sphinx

--- init.py
+++ init.py
@@ -1,12 +1,3 @@
-"""
-Программа DemoVisual демонстрирует 3 графика финансовых данных в виде так называемых
-свечей. Виджет первый использует Matplotlib, второй Seaborn, третий Plotly. Данные
-представлены в файле.csv, изменение цены BTC/USD и объема BTC. Цель - продемонстрировать
-возможности библиотек Matplotlib, Seaborn, Plotly по рисованию одних и тех же данных
-для сравнения. Дипломная работа представлена здесь:
-https://docs.google.com/document/d/1i4R_EbINhBnbfXReXiWJflzoM0yqXwr6ytVCRKLLQO8/edit?usp=sharing
-"""
-
 
 def main():
     pass
